32399e782d2506b655c0081c4c3949a5af4db6c6
f.atieh <fa===>
2019-12-10 12:51:56 +0200

    call sub method on main activity

--- app/src/main/java/com/example/testapp_github/MainActivity.java
+++ app/src/main/java/com/example/testapp_github/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        double result = subTwoNumbers(80, 14);
     }
 
     private int subTwoNumbers(int x, int y){
