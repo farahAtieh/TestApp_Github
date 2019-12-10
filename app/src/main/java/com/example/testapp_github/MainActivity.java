0ada9257ad5be20936e6010dad0230b0ded9d8d0
f.atieh <fa===>
2019-12-10 12:40:28 +0200

    add sub, add, mulm and div methods

--- app/src/main/java/com/example/testapp_github/MainActivity.java
+++ app/src/main/java/com/example/testapp_github/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private int subTwoNumbers(int x, int y){
+        return x - y;
+    }
+    private int addTwoNumbers(int x, int y){
+        return x + y;
+    }
+    private int mulTwoNumbers(int x, int y){
+        return x * y;
+    }
+    private int divTwoNumbers(int x, int y){
+        return x / y;
     }
 }
